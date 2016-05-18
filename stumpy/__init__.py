d2c62e8547923dd68a89df9c1bb24e6afedf6ea3
Andrew Kubera <andrewkubera@gmail.com>
2016-05-18 18:13:26 -0400

    __init__.py: Added enable_inline_ROOT_stuff for JSROOT support in ipython notebooks.

--- stumpy/__init__.py
+++ stumpy/__init__.py
@@ -5,3 +5,19 @@
 """
 
 from .histogram import Histogram
+
+
+def enable_inline_ROOT_stuff():
+    from IPython.display import Javascript, display
+    display(Javascript("""
+    console.log("enabling JSRoot")
+    requirejs.config({
+    paths: {JSRoot: [
+        '/files/post_analysis/jsroot/scripts/JSRootCore',
+        'https://raw.githubusercontent.com/linev/jsroot/master/scripts/JSRootCore'
+    ]}
+    });
+    require(["JSRoot"], function (ROOT) {
+        console.log("[stumpy::enable_inline_ROOT_stuff] Loaded JSRoot", ROOT);
+    })
+    """))
