1acea12c9d8b0eb2b2603c6b1b12668f866366b5
Andrew Kubera <andrew.michael.kubera@cern.ch>
2017-01-11 17:29:32 -0500

    __meta__.py: Arbitrary version update to 0.0.1; moved url to gitlab.cern.ch

--- stumpy/__meta__.py
+++ stumpy/__meta__.py
@@ -1,19 +1,19 @@
 #
-# __meta__.py
+# stumpy/__meta__.py
 #
 """Stumpy Metadata"""
 
 package = 'stumpy'
 description = 'A package to connect ROOT files to numpy projects.'
 
-version_info = (0, 0, 0)
+version_info = (0, 0, 1)
 version = '.'.join(map(str, version_info))
 
-date = "Jan 29, 2016"
+date = "Jan 11, 2017"
 author = "Andrew Kubera"
-author_email = "andrew.kubera@cern.ch"
-copyright = "Copyright 2016, Andrew Kubera"
+author_email = "andrew.michael.kubera@cern.ch"
+copyright = "Copyright 2017, Andrew Kubera"
 
-url = "https://github.com/akubera/stumpy"
+url = "https://gitlab.cern.ch/akubera/stumpy"
 
 license = 'MIT'
